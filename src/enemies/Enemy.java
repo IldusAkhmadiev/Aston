7a9c0436c98d8852f91a36409993dbf8d83b0523
Ildus Akhmadiev <anan308an@gmail.com>
2024-08-17 09:28:38 +0300

    add interface Mortal refactor Enemy implement Mortal

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -1,7 +1,11 @@
 package enemies;
 
-public class Enemy {
+import enemies.interfaces.Mortal;
+import heroes.Hero;
+
+public class Enemy implements Mortal {
     private int health;
+    private Hero hero;
 
     public Enemy(int health) {
         this.health = health;
@@ -17,5 +21,13 @@ public class Enemy {
 
     public void takeDamage(int damage) {
         health -= damage;
+    }
+
+    @Override
+    public boolean isAlive() {
+        if(health > 0) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/enemies/interfaces/Mortal.java
@@ -0,0 +1,5 @@
+package enemies.interfaces;
+
+public interface Mortal {
+    boolean isAlive();
+}
